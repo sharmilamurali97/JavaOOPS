595803c79b0835b45bd100d63845ad3a76459e92
sharmilamurail <sharmilamurali97@gmail.com>
2018-04-08 13:03:56 +0530

    Package

---
+++ package/src/pack/Package1.java
@@ -0,0 +1,9 @@
+package pack;
+public class Package1 {
+	public void chocklates() {
+		System.out.println("SWEET");
+		
+
+	}
+
+}
---
+++ package/src/pack2/package2.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import pack.Package1;//import the package1 in package2
+
+public class package2 {
+
+	public static void main(String[] args) {
+		Package1 load =new Package1();//object created for package1
+		load.chocklates();
+		
+
+	}
+
+}
+ 
