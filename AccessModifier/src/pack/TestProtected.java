8349b57a6a24b819c0a3db7d759f2e80e3aa57d2
sharmilamurail <sharmilamurali97@gmail.com>
2018-04-08 13:05:45 +0530

    AccessModifiers

---
+++ AccessModifier/src/pack/TestProtected.java
@@ -0,0 +1,10 @@
+package pack;
+public class TestProtected {
+protected void display() {//protected scope is within the package and 
+						  //also the subpackage with extended class only
+	System.out.println("SHOWING");
+}
+}
+	
+
+
---
+++ AccessModifier/src/pack1/TestProtected1.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import pack.TestProtected;
+
+public class TestProtected1 extends TestProtected {
+
+	public static void main(String[] args) {
+		TestProtected1 user =new TestProtected1();
+		user.display();
+		
+	}
+
+}
