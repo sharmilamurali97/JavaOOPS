8ca52cdb781c1fe71bed13fbf1c6093b336d138b
sharmilamurali97 <sharmilamurali97@gmail.com>
2018-04-14 12:29:30 +0530

    Default Access modifier

---
+++ AccessModifier/src/pack1/Default1.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import pack.Default;
+
+public class Default1 {
+		public static void main(String[] args) {
+			Default user =new Default();
+			user.display();//the method is in default so it cannot implement 
+			
+		
+
+	}
+
+}
