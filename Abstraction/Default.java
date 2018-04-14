c98c78fe826b9c18f0d2b419eaf2974089fdad6f
sharmilamurali97 <sharmilamurali97@gmail.com>
2018-04-14 12:36:55 +0530

    private

---
+++ Abstraction/Default.java
@@ -0,0 +1,14 @@
+package pack;
+public class Default {
+	void display() { // it is default method so it access only in this particular package 
+		System.out.println("SHOWING");
+	}
+}
+
+
+	
+		
+
+
+
+
